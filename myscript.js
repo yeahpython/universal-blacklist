b1bbb9bbfea0044fcae55b783790c5fc6f01c041
yeahpython <tommyli@stanford.edu>
2016-01-15 01:18:56 -0800

    Added loop

--- myscript.js
+++ myscript.js
@@ -1,7 +1,13 @@
-$("p:contains('the')")
-  .addClass("censorship-blur")
-  .hover(function(){
-  	$(this).removeClass("censorship-blur");
-  }, function(){
-  	$(this).addClass("censorship-blur");
-  });
+function enforceCensorship() {
+  $("p, span").filter(":contains('eugeni'):not(.censorship-blur, .censorship-hover)")
+    .addClass("censorship-blur")
+    .hover(function(){
+    	$(this).addClass("censorship-hover");
+        $(this).removeClass("censorship-blur");
+	  }, function(){
+	    $(this).addClass("censorship-blur");
+	    $(this).removeClass("censorship-hover");
+	  });
+  setTimeout(enforceCensorship, 1000);
+}
+enforceCensorship();
