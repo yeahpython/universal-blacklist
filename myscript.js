874a3c8304fc56cd585b1fd76c8253826b2f0976
yeahpython <tommyli@stanford.edu>
2016-01-16 14:26:16 -0800

    robust selectors, css filtering, comments

--- myscript.js
+++ myscript.js
@@ -1,69 +1,100 @@
+var semanticModules = "p, span, h1, h2, h3, h4, h5, h6, a, button" +
+", [class*=item], [class*=Item], [class*=entry], [class*=Entry]" +
+", .userContentWrapper"; // Facebook
+
+
+
+// Escape bad characters from user input.
 function escapeRegExp(str) {
   return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 }
+
+// This tracks how many times the regex has been modified on this page.
 var modcount = 0;
+
+// This will be a regex that matches blacklisted strings
 var re;
+
+// Set to true when the user updates the blacklist
 var regexNeedsUpdate = true;
+
+// Search page for occurences of words on the blacklist and blurs out certain elements that contain them.
+// Sets up hover handlers so that elements are unblurred on mouseover.
+// Loops once a second to account for with DOM changes initiated by the website.
 function enforceCensorship() {
+  // This is used to be able to determine when mouseover and mouseouts
   var ruleSetTime = modcount;
 
+  // Mostly toggles between .censorship-hover and .censorship-blur classes
   var mouseOverHandler = function(){
     if (ruleSetTime == modcount) {
       $(this).addClass("censorship-hover");
     } else {
-      // this is some horribly imperfect cleanup... whatever
+      // Removes self if it notices that it is outdated
       $(this).unbind('mouseover', mouseOverHandler);
     }
     $(this).removeClass("censorship-blur");
   };
 
+  // Mostly toggles between .censorship-hover and .censorship-blur classes
   var mouseOutHandler = function() {
     if (ruleSetTime == modcount) {
       $(this).addClass("censorship-blur");
     } else {
+      // Removes self if it notices that it is outdated
       $(this).unbind('mouseout', mouseOutHandler);
     }
     $(this).removeClass("censorship-hover");
   };
 
-  $("p, span")
+  // Find common low-level DOM elements, and add .censorship-blur class to them
+
+  $(semanticModules)
     .filter(":not(.censorship-blur, .censorship-hover)")
 	  .filter(function(){
 	  	return re.test($(this).text());
 	  })
     .addClass("censorship-blur")
     .hover(mouseOverHandler, mouseOutHandler);
+
   if (regexNeedsUpdate) {
-    // clear old censorship
+    // When blacklist is modified
+    // remove old blurs
     modcount += 1;
-    $("p, span").filter(".censorship-blur")
+    $(semanticModules).filter(".censorship-blur")
       .removeClass("censorship-blur");
-    $("p, span").filter(".censorship-hover")
+    $(semanticModules).filter(".censorship-hover")
       .removeClass("censorship-hover");
     setTimeout(makeRegexAndEnforceCensorship, 1000);
   } else {
+    // Loop once a second
     setTimeout(enforceCensorship, 1000);
   }
 }
 
+// Assembles a regex from stored blacklist
 function makeRegexAndEnforceCensorship() {
   chrome.storage.sync.get("triggers", function(items) {
     var bannedWords = items["triggers"];
     var escapedBannedWords = $.map(bannedWords, function(val, key) {
-      return escapeRegExp(key);
+      return "\\b" + escapeRegExp(key) + "\\b";
     });
     var regexString = escapedBannedWords.join("|");
     if (regexString == "") {
+      // Rejects everything
       regexString = "[^\\w\\W]";
     }
-    console.log("regexString is", regexString);
-    re = new RegExp(regexString);
+    re = new RegExp(regexString, "i");
     regexNeedsUpdate = false;
+    // Start blurring loop.
     enforceCensorship(re);
   });
 }
 
+// Initiates censorship loop.
 makeRegexAndEnforceCensorship();
+
+// When the blacklist changes the regex needs to be updated
 chrome.storage.onChanged.addListener(function(changes, namespace){
   regexNeedsUpdate = true;
 });
--- popup.js
+++ popup.js
@@ -1,15 +1,18 @@
 var input = document.getElementById("input");
 
+// Add the word from $("#input") to the stored blacklist
 function addWord() {
   var word = input.value;
+  // Get the stored blacklist
   chrome.storage.sync.get("triggers", function(items) {
   	var savedTriggers = items["triggers"];
-  	console.log(savedTriggers);
-  	if (savedTriggers) {
+  	// Add word to our copy of the blacklist
+    if (savedTriggers) {
   		savedTriggers[word] = true;
   	} else {
   		savedTriggers = {word: true};
   	}
+    // Set the blacklist with our modified copy
   	chrome.storage.sync.set({"triggers": savedTriggers}, function(){
       rerender();
       $("#response").html("Saved word " + word);
@@ -17,13 +20,17 @@ function addWord() {
     });
   });
 }
-
+// Add the word to the blacklist when the user click the add button
 $("#add").click(addWord);
+
+// Add the word to the blacklist when the user presses enter
 $("#input").keyup(function(e){
 	if (e.keyCode == 13) {
 		addWord();
 	}
 })
+
+// Remove any word in the blacklist that is clicked from the storage
 $("#triggers").click(function(e){
 	if ($(event.target).is("li")) {
 		var word = event.target.innerHTML;
@@ -40,11 +47,10 @@ $("#triggers").click(function(e){
 	}
 });
 
+// Shows a list of words generated from the blacklist.
 function rerender() {
-  //$("#triggers").html("");
   var list = $("<ul/>");
   chrome.storage.sync.get("triggers", function(items){
-  	console.log(items);
   	if (items["triggers"] && items["triggers"].length !== 0) {
   	  $.each(items["triggers"], function(currentValue, trueOrFalse){
    		  $("<li/>").html(currentValue).appendTo(list);
@@ -55,4 +61,6 @@ function rerender() {
     }
   });
 }
+
+// Initial render
 rerender();
